020eb6c125f143269ba3b1d79452b2153e436dbe
Alex Mendes <mendes.develop@gmail.com>
2020-02-07 11:12:41 -0500

    completed

---
+++ src/Cell.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+  constructor(props){
+    super(props)
+    console.log(props)
+    this.state = {
+      color: props.value
+    }
+  }
+
+  updateStateColor = () => {
+    this.setState({
+      color: '#333'
+    })
+  }
+
+
+
+  render(){
+    return (
+      <div className="cell"  style={{backgroundColor: this.state.color}} onClick={this.updateStateColor}></div>
+    )
+  }
+
+}
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Matrix extends Component {
+
   
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
-  genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
-  
-  render() {
+    // vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map((val, index) => <Cell key={index} value={val} />)
+    )
+    
+    genMatrix = () =>  (
+      this.props.values.map((rowVals, index) => <div key={index} className="row">{this.genRow(rowVals)}</div>)
+      )
+      
+      render() {
+      console.log(this.props.values)
     return (
       <div id="matrix">
         {this.genMatrix()}
@@ -18,4 +22,19 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: [
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+  ]
 }
